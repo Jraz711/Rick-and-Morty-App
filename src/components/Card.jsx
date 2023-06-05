e2c88eefc5937059965dfe391b1f2dedafc570a7
Johann Rodriguez Abreu <johann2464@gmail.com>
2023-06-05 15:59:24 -0400

    Maquetacion Faltante

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
 import useFetch from "../hooks/useFetch"
+import "./styles/card.css"
+
 
 const Card = ({ url }) => {
 
@@ -10,20 +12,28 @@ const Card = ({ url }) => {
   }, [])
 
   return (
-    <article>
-      <header>
-        <img src={resident?.image} alt="" />
-        <div>
-          <span></span>
-          <span>{resident?.status}</span>
+    <article className="resident">
+      <header className="resident_header">
+        <img
+          className="resident_img"
+          src={resident?.image}
+          alt="" />
+        <div className="resident_status">
+          <span className={`resident_cicle ${resident?.status}`}></span>
+          <span className="resident_status-value">
+            {resident?.status}</span>
         </div>
       </header>
-      <section>
-        <h3>{resident?.name}</h3>
-        <ul>
-          <li><span>Specie</span><span>{resident?.species}</span></li>
-          <li><span>Origin</span><span></span>{resident?.origin.name}</li>
-          <li><span>Eppisodes Where Apperar</span><span>{resident?.episode.length}</span></li>
+      <section className="resident_body">
+        <h3 className="resident_name">{resident?.name}</h3>
+        <hr className="resident_hr" />
+        <ul className="resident_list">
+          <li className="resident_item"><span className="resident_label">Specie</span>
+            <span className="resident_value">{resident?.species}</span></li>
+          <li className="resident_item"><span className="resident_label">Origin</span>
+            <span className="resident_value"></span>{resident?.origin.name}</li>
+          <li className="resident_item"><span className="resident_label">Eppisodes Where Apperar</span>
+            <span className="resident_value">{resident?.episode.length}</span></li>
         </ul>
       </section>
     </article>
--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,23 +1,49 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 const InputSearch = ({ setidLocation }) => {
 
+
   const idLocationValue = useRef()
+
+  const [inputIsEmpty, setinputIsEmpty] = useState(false)
 
   const searchLocation = e => {
     e.preventDefault()
-    setidLocation(idLocationValue.current.value.trim())
+    const inputValue = idLocationValue.current.value.trim()
+
+    if (inputValue === '') {
+      setinputIsEmpty(true)
+      setTimeout(() => {
+        setinputIsEmpty(false)
+      }, 3000)
+    }
+    else {
+      setidLocation(inputValue)
+    }
+
+    // if (inputValue !== '') {
+    //   setidLocation()
+
+    // }
+
+
+
   }
 
   return (
-    <form onSubmit={searchLocation} className='input'>
-      <input
-        type="text"
-        placeholder='Input a dimension number from 1 to 126'
-        ref={idLocationValue}
-      />
-      <button>Search</button>
-    </form>
+    <>
+      <form onSubmit={searchLocation} className='input'>
+        <input
+          type="text"
+          placeholder='Input a dimension number from 1 to 126'
+          ref={idLocationValue}
+        />
+        <button>Search</button>
+      </form>
+      {
+        inputIsEmpty && <h3>This FIeld is requiered. 👀</h3>
+      }
+    </>
   )
 }
 
